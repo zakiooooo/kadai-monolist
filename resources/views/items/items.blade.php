a1ad5f67754de34f2650b6837b06ec970419c3d9
jiro.kirameki <ec2-user@ip-172-31-35-132.us-east-2.compute.internal>
2018-06-15 06:11:50 +0000

    have ranking

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,6 +1,6 @@
 @if ($items)
     <div class="row">
-        @foreach ($items as $item)
+        @foreach ($items as $key => $item)
             <div class="item">
                 <div class="col-md-3 col-sm-4 col-xs-12">
                     <div class="panel panel-default">
@@ -20,6 +20,12 @@
                                 @endif
                             </div>
                         </div>
+                        @if (isset($item->count))
+                            <div class="panel-footer">
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} {{$type}}</p>
+                                <!--<p class="text-center">{{ $key+1 }}位: {{ $item->count}} Haves</p>-->
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
